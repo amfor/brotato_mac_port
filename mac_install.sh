3749fe48d86a9490bcd8bdbc61e821d0d0aa555a
Amy <aryawa@icloud.com>
2024-10-25 22:02:59 -0400

    Bump godot 3.6, use download_depot since game appears incompatible on MacOS.

--- mac_install.sh
+++ mac_install.sh
@@ -1,4 +1,6 @@
 export APP_ID=1942280 # Brotato Steam App ID
+export BUILD_ID=1942281 # https://steamdb.info/depot/1942281/
+export MANIFEST_ID=2496830421398627117 # Pre-DLC compatibility 
 
 source pathing.sh
 
@@ -15,13 +17,14 @@ echo "Enter your SteamGuard Code"
 read STEAM_CODE
 
 CURRENT_DIR = $(pwd)
-./steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir $MAC_GAME_FOLDER +login $STEAM_USERNAME $STEAM_PASSWORD $STEAM_CODE +app_update $APP_ID validate +exit
+./steamcmd.sh +@sSteamCmdForcePlatformType windows +login $STEAM_USERNAME $STEAM_PASSWORD $STEAM_CODE +download_depot $APP_ID $BUILD_ID $MANIFEST_ID validate +exit
+cp -rf steamapps/content/app_1942280/depot_1942281 $MAC_GAME_FOLDER/
 
 mkdir godot_files && cd godot_files
 # Download Latest Release (g353 at time of writing)
 # See https://github.com/Gramps/GodotSteam/releases for latest release
-curl -L -O https://github.com/GodotSteam/GodotSteam/releases/download/v3.26/godotsteam-g353-s160-gs326-templates.zip
-unzip godotsteam-g353-s160-gs326-templates.zip
+curl -L -O https://github.com/GodotSteam/GodotSteam/releases/download/v3.27/godotsteam-g36-s160-gs327-templates.zip
+unzip godotsteam-g36-s160-gs327-templates.zip
 unzip macos.zip
 
 # Copy godot_osx_release.64 and libsteam_api.dylib into Brotato folder
--- pathing.sh
+++ pathing.sh
@@ -4,5 +4,5 @@ MAC_GAME_FOLDER="/Users/$USER/Steam/brotato"
 MAC_SAVE_FOLDER="/Users/$USER/Library/Application Support/Brotato"
 # The folder where our saves live for the Windows Install. In this case, I'm using CrossOver
 # This is what you'd need to change if you use a different solution (ex: Parallels)
-BOTTLE_NAME=Steam
+BOTTLE_NAME=Win10
 SYNCED_SAVE_FOLDER="/Users/$USER/Library/Application Support/CrossOver/Bottles/$BOTTLE_NAME/drive_c/users/crossover/AppData/Roaming/Brotato/76561198017744487"
